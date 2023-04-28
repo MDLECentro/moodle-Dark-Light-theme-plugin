fba33cf760f397df97d8daa50b537d073c1380d1
MDLECentro <132074845+MDLECentro@users.noreply.github.com>
2023-04-28 19:09:50 +0500

    version.php

---
+++ local/themecolorswitcher/version.php
@@ -0,0 +1,7 @@
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_themecolorswitcher';
+$plugin->version   = 2023042800;
+$plugin->requires  = 2018051700; // Requires Moodle 3.5.
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.0';
